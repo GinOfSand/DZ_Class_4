1b965a598fb51f08c11e8c28b41153a3b57236c2
GinOfSand <96676512+GinOfSand@users.noreply.github.com>
2022-07-06 18:32:57 +0700

    Добавьте файлы проекта.

---
+++ DZ_Class_4/Market.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Class_4
+{
+    internal class Market
+    {
+        public string marketName { get; set; }
+        public string Adres { get; set; }
+        public string specializationMarket { get; set; }
+        public string emailM { get; set; }
+        public string phoneNumber { get; set; }
+        public Market(string marketName = "Market", string adres = "dungeons1", string specializationMarket  = "souleater",
+            string emailM = "12345@mail.ru", string phoneNumber="+7-923-777-96-75")
+        {
+            this.marketName = marketName;
+            Adres = adres;
+            this.specializationMarket = specializationMarket;
+            this.emailM = emailM;
+            this.phoneNumber = phoneNumber;
+        }
+       public override string ToString()
+        { 
+            string result ="Название магазина : "+ marketName + "\n" + "Адресс : " + Adres + 
+                "\n"+"Профиль магазина : " + specializationMarket + "\n" + "Электронная почта : " + emailM + "\n" +"Номер телефона : "+ phoneNumber;
+            return result;
+        }
+    }
+}
---
+++ DZ_Class_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Class_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Market a = new Market();
+            Console.WriteLine(a);
+            a.marketName = "Святзарск";
+            a.specializationMarket = "Садоводческий магазин";
+            a.Adres = "Слоновий юл 36";
+            a.emailM = "sv9tsv9t@zors.mail.ru";
+            a.phoneNumber = "+7-924-236-2378";
+            Console.WriteLine();
+            Console.WriteLine(a);
+        }
+    }
+}
